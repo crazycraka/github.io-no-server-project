71a8cdf5e2350900f3bafeb084918743955ee61c
Scott Schermerhorn <plaidscott@gmail.com>
2016-12-01 23:45:56 -0700

    added config to housingApp.js to try and coerce relative path use in gitpages

--- js/housingApp.js
+++ js/housingApp.js
@@ -1,4 +1,6 @@
 /**
  * Created by scott on 12/1/16.
  */
-var app = angular.module('housingApp', [])
+var app = angular.module('housingApp', []).config(function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+});
