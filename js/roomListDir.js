e64fd7265f5e423b345a6af33f7c19bf2786bf5d
Scott Schermerhorn <plaidscott@gmail.com>
2016-12-05 17:12:01 -0700

    mostly functional, can add apt complex, apt rooms, and room data. Input boxes reset to display an empty box, and check boxes available to open individual rooms, or all rooms of an apartment complex

--- js/roomListDir.js
+++ js/roomListDir.js
@@ -2,34 +2,53 @@
  * Created by scott on 12/1/16.
  */
 
-angular.module('housingApp').directive('roomListDir', function(){
+angular.module('housingApp').directive('roomListDir', function () {
 
     return {
         templateUrl: './html/roomList.html',
         scope: {
             rooms: '='
         },
-        link: function(scope, element, attributes){
+        link: function (scope, element, attributes) {
 
-            scope.addRoom = function(rooms, roomId) {
+            scope.addRoom = function (rooms, roomId, roomCount, bathCount, kitchenApp) {
 
-                 var newRoom = rooms.rooms.push({ roomId : roomId } );
-                scope.housing = JSON.parse( localStorage.getItem( 'housing' ) )
+                var newRoom = rooms.rooms.push({});
+                scope.housing = JSON.parse(localStorage.getItem('housing'))
 
                 var trackAptIndex;
-                for(var i = 0; i < scope.housing.length; i++){
-                    if(scope.housing[i].apt === rooms.apt){
+                for (var i = 0; i < scope.housing.length; i++) {
+                    if (scope.housing[i].apt === rooms.apt) {
                         trackAptIndex = i;
                     }
                 }
 
-                scope.housing[trackAptIndex].rooms.push( { roomId : roomId } )
-                localStorage.setItem( 'housing' , JSON.stringify( scope.housing ) );
+                scope.housing[trackAptIndex].rooms.push({
+                    roomId: roomId,
+                    roomInfo: {
+                        roomCount: roomCount,
+                        bathCount: bathCount,
+                        kitchenApp: kitchenApp
+                } } );
+
+                console.log(scope.housing);
+                localStorage.setItem('housing', JSON.stringify(scope.housing));
+
 
 
                 // reset the input text
                 scope.roomId = '';
-            }
+                scope.roomCount = '';
+                scope.bathCount = '';
+                scope.kitchenApp = '';
+                console.log(rooms);
+            };
+            // END OF ADDROOM
+
+            scope.roomSlider = function () {
+                element.children.toggleClass('room-slide');
+            };
+            // END OF ROOMSLIDER
 
 
         }
